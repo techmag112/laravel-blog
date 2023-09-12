04b55f1bd870922348d68588b641a11a9bf87145
techmag112 <oleg.khayrullin@gmail.com>
2023-09-12 14:39:39 +0300

    chore(root): Add frontend libs
    add tailwind, tailwind/forms

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\PDO\Connection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(!app()->isProdaction);
+        Model::preventsSilentlyDiscardingAttributes();
+        DB::whenQueryingForLongerThan(500, function (Connection $connection){
+
+        });
     }
 }
